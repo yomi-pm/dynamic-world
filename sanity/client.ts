d19041d190cac1a284e95742695d7b7e26cdb986
yomi-pm <yomi@theworkflowlab.com>
2026-06-10 11:16:38 +0100

    Launch version

--- sanity/client.ts
+++ sanity/client.ts
@@ -3,6 +3,6 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  apiVersion: "2025-01-01",
   useCdn: true,
 });
