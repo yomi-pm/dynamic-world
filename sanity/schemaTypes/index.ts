187cc09af8f501ed97d4bf0cb7e88cf558c669f7
yomi-pm <yomi@theworkflowlab.com>
2026-06-12 10:11:33 +0100

    Add New Topics

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,23 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+
+import projectTopic from "./projectTopic";
+import blogPost from "./blogPost";
+import caseStudy from "./caseStudy";
+import resource from "./resource";
+import service from "./service";
+import researchGuide from "./researchGuide";
+import researchTemplate from "./researchTemplate";
+import statisticalTest from "./statisticalTest";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [
+    projectTopic,
+    blogPost,
+    caseStudy,
+    resource,
+    service,
+    researchGuide,
+    researchTemplate,
+    statisticalTest,
+  ],
+};
